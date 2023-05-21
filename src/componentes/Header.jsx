c87749880bf8c4afd0fa562c2bf2c975b5dd0b81
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-05-21 12:39:23 -0300

    tercer commit

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,6 +1,7 @@
 
 import "./Header.css"
-import Img1 from "./../../public/imagenes/portadaAdaptative.png"
+import Img1 from "./../../public/imagenes/portadaAdaptative3.png"
+
 
 const Header = () => {
     return (
@@ -8,8 +9,11 @@ const Header = () => {
            <div className="header1">
             <div className="boximg">
                <div><img src={Img1} alt="" /></div> 
-               
+               <div className="parrafo"><p>QUE COMIENCE LA AVENTURA</p><button className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+  Button
+</button>
             </div>
+               </div>
             </div> 
         </div>
     );
