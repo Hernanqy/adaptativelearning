16a89e10206ecfc57a22e526a19c125c8979c210
Hernanqy <desarrolladoryotti@gmail.com>
2023-05-22 16:48:19 -0300

    acomdando header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -8,19 +8,21 @@ const Header = () => {
     return (
         <div>
             <div className="cuerpo">
-           <div className="header1 " >
-            <div className="boximg">
-               <div className="grow"><img className="img1" src={Img1} alt="" /></div> 
-               <div className="parrafo"> <div className="container">
-               <div className="title">
- <h1>Comienza tu Aventura</h1>
 
- 
+           <div className="header1 " >
+
+            <div className="boximg">
+
+               <div className="boxImg1" ><img className="img1" src={Img1} alt="" /></div> 
+               <div className="parrafo"> <div className="container">
+               <div className="title grow">
+ <h1>Comienza tu Aventura</h1>
 </div>
 </div><button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
   Button
 </button>
             </div>
+
              <div className="pantallaGrande"><img className="img2" src={Img2} alt="" /></div>
                </div>
               
