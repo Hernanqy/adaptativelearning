4d59af1ae925bba6394f649029cb9675433b3b5c
Hernanqy <desarrolladoryotti@gmail.com>
2023-05-21 23:09:48 -0300

    cuarto commit

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,19 +1,22 @@
 
 import "./Header.css"
 import Img1 from "./../../public/imagenes/portadaAdaptative3.png"
+import Img2 from "./../../public/imagenes/avatar8.png"
 
 
 const Header = () => {
     return (
         <div>
-           <div className="header1">
+           <div className="header1 " >
             <div className="boximg">
-               <div><img src={Img1} alt="" /></div> 
-               <div className="parrafo"><p>QUE COMIENCE LA AVENTURA</p><button className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+               <div className="grow"><img src={Img1} alt="" /></div> 
+               <div className="parrafo"><p className="text-center">QUE COMIENCE LA AVENTURA</p><button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
   Button
 </button>
             </div>
+             <div className="pantallaGrande"><img src={Img2} alt="" /></div>
                </div>
+              
             </div> 
         </div>
     );
