a19ce51816df05fdae517082e3974a0904fa3d56
Hernanqy <desarrolladoryotti@gmail.com>
2023-05-22 16:16:37 -0300

    commit cinco

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -12,16 +12,11 @@ const Header = () => {
             <div className="boximg">
                <div className="grow"><img className="img1" src={Img1} alt="" /></div> 
                <div className="parrafo"> <div className="container">
-	<div className="title">
-		<h1>COMIENZA TU AVENTURA</h1>
-	</div>
-	<div className="ghost">
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
+               <div className="title">
+ <h1>Comienza tu Aventura</h1>
+
+ 
+</div>
 </div><button className="bg-blue-500 grow hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
   Button
 </button>
