7da1d5f97eaf79fc7c971dad06a1521368cca0cb
Hernanqy <desarrolladoryotti@gmail.com>
2023-05-24 19:54:54 -0300

    modificacionspan

--- src/componentes/Navb.jsx
+++ src/componentes/Navb.jsx
@@ -8,7 +8,10 @@ const Navb = () => {
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="/" className="flex items-center">
       <img src={videoLogo} className="h-10 mr-3" alt="Flowbite Logo" />
+      <div className="spanLogo">
       <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">SIMON</span>
+      <span className="self-center text-1xl font-semibold whitespace-nowrap text-white">Secundarias Profesionales</span>
+      </div>
   </a>
   <div className="flex md:order-2">
       <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Intrucciones</button>
