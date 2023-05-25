a797fd8d82663e0bd375038b6cad4af1ea12d721
Hernanqy <desarrolladoryotti@gmail.com>
2023-05-25 10:31:22 -0300

    navegacion

--- src/componentes/Hardware.jsx
+++ src/componentes/Hardware.jsx
@@ -4,9 +4,27 @@ import "./Hardware.css"
 const Hardware = () => {
     return (
         <div>
+            <div className="container">
+  <svg viewBox="0 0 960 300">
+    <symbol id="s-text">
+      <text textAnchor="middle" x="50%" y="80%">DESAFIO 1</text>
+    </symbol>
+
+    <g className = "g-ants">
+      <use xlinkHref="#s-text" className="text-copy"></use>
+      <use xlinkHref="#s-text" className="text-copy"></use>
+      <use xlinkHref="#s-text" className="text-copy"></use>
+      <use xlinkHref="#s-text" className="text-copy"></use>
+      <use xlinkHref="#s-text" className="text-copy"></use>
+    </g>
+  </svg>
+</div>
+
             <div className="contenedorVideo">
 
                 <div>
+
+                    
 
                     <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={Img10} alt="" />
