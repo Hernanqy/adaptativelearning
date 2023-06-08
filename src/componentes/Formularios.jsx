ef243433b94edf990d0b85be4c77727da3707448
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-06-08 13:21:35 -0300

    AVANZANDO

--- src/componentes/Formularios.jsx
+++ src/componentes/Formularios.jsx
@@ -5,6 +5,7 @@ const Formularios = () => {
         <div >
             <div className="formulario">
            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScjrHG4sDjXVLTJsnQyMDF-bKlFAjV7_yN2PuTqxStg_IsFbg/viewform?embedded=true" width="640" height="2065" >Cargando…</iframe>
+           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSellKhr7qGe-l-2oid2N0XisX9xC91DHWHOQ_jBXXnIqyBVKg/viewform?embedded=true" width="640" height="769" >Cargando…</iframe>
         </div>
         </div>
     );
