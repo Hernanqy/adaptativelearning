a3a65990ada6dc37ec4d83f38377f9783c40b005
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-06-10 17:46:44 -0300

    sendero

--- src/componentes/ElSendero.jsx
+++ src/componentes/ElSendero.jsx
@@ -1,10 +1,12 @@
 import "./ElSendero.css"
+import ImgConector1 from "../../public/imagenes/ImgConector1.png"
 
 function ElSendero() {
   return (
     <div className="contenedorSendero">
-      <div ><iframe title="EL SENDERO <>"  width="1200" height="675" src="https://view.genial.ly/647d19d1fe2cb500128d3df3" type="text/html" allowfullscreen="true" ></iframe> </div> 
-        
+      <div ><iframe title="EL SENDERO <>"  width="600" height="375" src="https://view.genial.ly/647d19d1fe2cb500128d3df3" type="text/html" allowfullscreen="true" ></iframe> </div> 
+      <div><img src={ImgConector1} alt="" /></div>
+      <div ><iframe title="nivel 2"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe> </div> 
     </div>
   )
 }
