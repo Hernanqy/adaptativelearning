487f7e8edab6b9d1e486202cf156002dab2affa0
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-06-11 22:38:12 -0300

    nuevo sendero

--- src/componentes/ElSendero.jsx
+++ src/componentes/ElSendero.jsx
@@ -1,20 +1,17 @@
 import "./ElSendero.css"
-import ImgConector1 from "../../public/imagenes/ImgConector1.png"
+
+import Nivel1 from "../../public/imagenes/Nivel1.gif"
 
 function ElSendero() {
   return (
     < > 
       
-      <div className="contenedorSendero">
-      <div ><iframe title="EL SENDERO <>"  width="600" height="375" src="https://view.genial.ly/647d19d1fe2cb500128d3df3" type="text/html" allowfullscreen="true" ></iframe> </div> 
-      <div><img src={ImgConector1} alt="" /></div>
-      <div ><iframe title="nivel 2"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe> </div> 
+      <div >
+      <div className="contenedorSendero" ><img src={Nivel1} alt="" /> </div> 
+     
+     
    </div>
-      <div className="contenedorSendero">
-      <div > <iframe title="nivel 3"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe></div> 
-      <div><img src={ImgConector1} alt="" /></div>
-      <div ><iframe title="nivel 2"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe> </div> 
-   </div>
+     
    
    
     </>
