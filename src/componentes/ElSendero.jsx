584b9a42d306f82e6adf272a3a6bb2db33515442
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-06-11 21:26:03 -0300

    avance de hoy

--- src/componentes/ElSendero.jsx
+++ src/componentes/ElSendero.jsx
@@ -3,11 +3,21 @@ import ImgConector1 from "../../public/imagenes/ImgConector1.png"
 
 function ElSendero() {
   return (
-    <div className="contenedorSendero">
+    < > 
+      
+      <div className="contenedorSendero">
       <div ><iframe title="EL SENDERO <>"  width="600" height="375" src="https://view.genial.ly/647d19d1fe2cb500128d3df3" type="text/html" allowfullscreen="true" ></iframe> </div> 
       <div><img src={ImgConector1} alt="" /></div>
       <div ><iframe title="nivel 2"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe> </div> 
-    </div>
+   </div>
+      <div className="contenedorSendero">
+      <div > <iframe title="nivel 3"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe></div> 
+      <div><img src={ImgConector1} alt="" /></div>
+      <div ><iframe title="nivel 2"  width="600" height="375"  src="https://view.genial.ly/6484dcf7509133001b99b181" type="text/html"  allowfullscreen="true" ></iframe> </div> 
+   </div>
+   
+   
+    </>
   )
 }
 
