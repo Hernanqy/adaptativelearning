0ba5b7f6925f36bf2334e05d7a4618749cd9e445
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-06-16 17:23:39 -0300

    NUEVOSNIVELS

--- src/componentes/ElSendero.jsx
+++ src/componentes/ElSendero.jsx
@@ -1,6 +1,12 @@
 import "./ElSendero.css"
-import ImghHabilidades from "../../public/imagenes/Habilidades digitales.jpg"
-
+import ImghHabilidades from "../../public/imagenes/NIVEL1.jpg"
+import ImghHabilidades1 from "../../public/imagenes/NIVEL2.jpg"
+import ImghHabilidades2 from "../../public/imagenes/NIVEL3.jpg"
+import ImghHabilidades3 from "../../public/imagenes/NIVEL4.jpg"
+import Imgpersonaje1 from "../../public/imagenes/Imgpersonaje1.png"
+import Imgpersonaje2 from "../../public/imagenes/Imgpersonaje2.png"
+import Imgpersonaje3 from "../../public/imagenes/Imgpersonaje3.png"
+import Imgpersonaje4 from "../../public/imagenes/Imgpersonaje4.png"
 
 function ElSendero() {
   return (
@@ -9,22 +15,28 @@ function ElSendero() {
       <div className="contenedorSendero" >
       
         <img src={ImghHabilidades} alt="" />
+        <img className="Imgpersonaje1" src={Imgpersonaje1} alt="" />
       </div>
 
 
 
 
       <div className="contenedorSendero1" >
-      
-        <img src={ImghHabilidades} alt="" />
+      <img className="Imgpersonaje1" src={Imgpersonaje2} alt="" />
+        <img src={ImghHabilidades1} alt="" />
       </div>
+
+
       <div className="contenedorSendero" >
       
-        <img src={ImghHabilidades} alt="" />
+        <img src={ImghHabilidades2} alt="" />
+        <img className="Imgpersonaje1" src={Imgpersonaje3} alt="" />
       </div>
+
+
       <div className="contenedorSendero1" >
-      
-        <img src={ImghHabilidades} alt="" />
+      <img className="Imgpersonaje1" src={Imgpersonaje4} alt="" />
+        <img src={ImghHabilidades3} alt="" />
       </div>
      
      
